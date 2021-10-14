b171d1faa5588f6ef364e7f1c1a32f415d92c23e
samanthastaheli <sta18011@byui.edu>
2021-10-14 14:37:25 -0600

    finished word class

--- 05-jumper/jumper/game/words.py
+++ 05-jumper/jumper/game/words.py
@@ -7,23 +7,18 @@ class Words():
     def __init__(self):
 
         self.words = ["Mars", "astronaut", "space", "comet", "explore", "supernova", "telescope", "Pluto"]
-<<<<<<< HEAD
         self.current_word = random.choice(self.words)
 
-
-=======
-        
->>>>>>> 1068fd3b8de1dd3bc7cb90473b2229a141e0f112
     def split_word(self):
         """
         Splits the word into a list of individual letters.
         """
         letter_list = list(self.current_word)
-        print(letter_list)
+        return letter_list
 
     def word_length(self):
         """
         Determines the length of the word.
         """
-
+        return len(self.current_word)
     
