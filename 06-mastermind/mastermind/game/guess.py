c972bdac7c7bba6b55e6f26cab8a2a5c8b3f4ae6
Jonathan <jlgunderson2@gmail.com>
2021-10-20 11:08:59 -0600

    pushed changes

--- 06-mastermind/mastermind/game/guess.py
+++ 06-mastermind/mastermind/game/guess.py
@@ -1,6 +1,9 @@
+#called once at start
+
 class Guess():
     def __init__(self):
         pass
+
 
     def get_guess(self):
         pass
--- 06-mastermind/mastermind/game/player.py
+++ 06-mastermind/mastermind/game/player.py
@@ -1,3 +1,5 @@
+
+
 class Player:
     """A person taking part in a game. The responsibility of Player is to keep track of their identity and last move.
     
